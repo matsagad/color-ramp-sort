62980559842767a8bce516b552a63bee841e789f
matsagad <matsagad@gmail.com>
2021-12-04 00:16:25 +0000

    Added file to handle hierarchical clustering.

---
+++ src/clustering.lua
@@ -0,0 +1,13 @@
+dofile("./tree.lua")
+
+-- TODO: implement hierarchical clustering
+
+-- Average Linkage Hierarchical Clustering
+local function get_average_linkage(cluster_a, cluster_b, distance_matrix)
+end
+
+local function get_dendogram(distance_matrix)
+end
+
+local function get_ramps_with_hierarchical_clustering(distance_matrix)
+end
